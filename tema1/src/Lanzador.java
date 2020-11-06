0d8cf111ae443b63b72cac697e37e32f366a58e9
RubenPalacian <47255940+RubenPalacian@users.noreply.github.com>
2020-11-06 20:01:31 +0000

    Add files via upload

---
+++ tema1/src/Lanzador.java
@@ -0,0 +1,23 @@
+public class Lanzador {
+    public void lanzarSumador(Integer n1,
+                              Integer n2){
+        String clase="com.ies.Sumador";
+        ProcessBuilder pb;
+        try {
+            pb = new ProcessBuilder(
+                    "java",clase,
+                    n1.toString(),
+                    n2.toString());
+            pb.start();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args){
+        Lanzador l=new Lanzador();
+        l.lanzarSumador(1, 30);
+        l.lanzarSumador(70, 100);
+        System.out.println("Ok");
+    }
+}
